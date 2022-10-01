877c8051a5e54845f86db0000f8d79b672771459
Club Of Programmers <114017269+STI-COPS@users.noreply.github.com>
2022-10-01 22:57:42 +0800

    Add files via upload

---
+++ Club Entries/MACARAIG_GROUP/bioJava.js
@@ -0,0 +1,15 @@
+function a(){
+
+    document.getElementById("img").src= "picutre/pic1.jpg" ;
+
+    var name="Name: Joshua Dimaunahan";
+
+    document.getElementById("name1").innerHTML= name;
+}
+
+function b(){
+    document.getElementById("img").src= "picutre/pic2.jpg" ;
+
+    var name="Name: Alias 	&#34;Baby Josh&#34;";
+    document.getElementById("name1").innerHTML= name;
+}
